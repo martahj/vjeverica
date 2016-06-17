0d0e338841bb9baa45d537d01fe8eb831e72c1ac
Marta Johnson <marta.hourigan.johnson@gmail.com>
2016-06-17 09:33:35 -0500

    switched to node 6

---
+++ server/models/modelHelper.js
@@ -0,0 +1,8 @@
+const db = require('../db.js');
+
+const modelHelper = {};
+module.exports = modelHelper;
+
+modelHelper.createModel = (tableName, ...attrs) {
+	
+}
---
+++ test/server/models/user_test.js
@@ -0,0 +1,14 @@
+"use strict"
+
+const helper = require('../../helpers/helper.js');
+const db = helper.db;
+
+describe('******* User model ******* ', function() {
+
+	before(function() {
+	  // return db.deleteEverything()
+	})
+
+	//
+
+})
