3590300009a34e59f4c3976d07e1c2ec2f8c0a05
Marta Johnson <marta.hourigan.johnson@gmail.com>
2016-07-16 10:35:32 -0500

    paragraph

--- components/main/paragraph.jsx
+++ components/main/paragraph.jsx
@@ -11,30 +11,3 @@ const Paragraph = ({clickFunction, paragraph}) => {
 }
 
 export default Paragraph;
-
-// class Paragraph extends Component {
-
-// 	// onParagraphClick () {
-
-// 	// }
-
-// 	render() {
-// 		return (
-// 			<p onClick={ () => this.props.clickFunction() }>
-// 			  {this.props.paragraph.text}
-// 			</p>
-// 		)
-// 	}
-// }
-
-
-
-// const mapStateToProps = (state) => {
-//   return state;
-// }
-
-// const mapDispatchToProps = (dispatch) => {
-// 	return {};
-// }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Paragraph);
