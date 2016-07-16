9503118923636c937cbae19ab55d7a4fe8429f4e
Marta Johnson <marta.hourigan.johnson@gmail.com>
2016-07-16 10:34:14 -0500

    made paragraph presentational

--- components/main/paragraph.jsx
+++ components/main/paragraph.jsx
@@ -1,29 +1,40 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
+// import {connect} from 'react-redux';
 
-class Paragraph extends Component {
 
-	// onParagraphClick () {
-
-	// }
-
-	render() {
-		return (
-			<p onClick={ () => this.props.clickFunction() }>
-			  {this.props.paragraph.text}
-			</p>
-		)
-	}
+const Paragraph = ({clickFunction, paragraph}) => {
+  return (
+    <p onClick={ () => clickFunction()}>
+      {paragraph.text}
+    </p>
+  )
 }
 
+export default Paragraph;
+
+// class Paragraph extends Component {
+
+// 	// onParagraphClick () {
+
+// 	// }
+
+// 	render() {
+// 		return (
+// 			<p onClick={ () => this.props.clickFunction() }>
+// 			  {this.props.paragraph.text}
+// 			</p>
+// 		)
+// 	}
+// }
 
 
-const mapStateToProps = (state) => {
-  return state;
-}
 
-const mapDispatchToProps = (dispatch) => {
-	return {};
-}
+// const mapStateToProps = (state) => {
+//   return state;
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Paragraph);
+// const mapDispatchToProps = (dispatch) => {
+// 	return {};
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Paragraph);
